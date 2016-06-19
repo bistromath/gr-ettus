de1f1740f6cbcac48512475d969dbe84291523aa
Nick Foster <bistromath@gmail.com>
2016-06-19 14:32:38 -0700

    Working

---
+++ lib/rfnoc_predistort_cci_impl.h
@@ -0,0 +1,46 @@
+/* -*- c++ -*- */
+/* Copyright 2015 Ettus Research
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * gr-ettus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with gr-ettus; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef INCLUDED_ETTUS_RFNOC_PREDISTORT_CCI_IMPL_H
+#define INCLUDED_ETTUS_RFNOC_PREDISTORT_CCI_IMPL_H
+
+#include <ettus/rfnoc_predistort_cci.h>
+#include "rfnoc_block_impl.h"
+
+namespace gr {
+  namespace ettus {
+
+    class rfnoc_predistort_cci_impl : public rfnoc_predistort_cci, public rfnoc_block_impl
+    {
+     public:
+      rfnoc_predistort_cci_impl(
+          const std::vector<std::vector<int> > &taps,
+          const device3::sptr &dev,
+          const int block_select,
+          const int device_select
+      );
+      ~rfnoc_predistort_cci_impl();
+
+      void set_taps(const int which, const std::vector<int> &taps);
+    };
+
+  } // namespace ettus
+} // namespace gr
+
+#endif /* INCLUDED_ETTUS_RFNOC_FIR_CCI_IMPL_H */
--- swig/ettus_swig.i
+++ swig/ettus_swig.i
@@ -54,6 +54,7 @@
 #include "ettus/rfnoc_window_cci.h"
 #include "ettus/rfnoc_radio.h"
 #include "ettus/rfnoc_generic.h"
+#include "ettus/rfnoc_predistort_cci.h"
 %}
 
 %include "ettus/rfnoc_block.h"
@@ -148,3 +149,5 @@ GR_SWIG_BLOCK_MAGIC2(ettus, rfnoc_window_cci);
 GR_SWIG_BLOCK_MAGIC2(ettus, rfnoc_radio);
 %include "ettus/rfnoc_generic.h"
 GR_SWIG_BLOCK_MAGIC2(ettus, rfnoc_generic);
+%include "ettus/rfnoc_predistort_cci.h"
+GR_SWIG_BLOCK_MAGIC2(ettus, rfnoc_predistort_cci);
